303d86513c85b941e13ec5e807de102796ceeb8b
正野敦也 <ShonoAtsuya@setalabnoMac-mini.local>
2017-11-22 18:38:19 +0900

    PHPでmysqlいじれるようにした、ホーム画面作成

--- connect_db.php
+++ connect_db.php
@@ -1,7 +1,7 @@
 <?php
 //データベースに接続
 try {
-    $sql = new PDO ( 'mysql:dbname = sample_db; host=localhost;port=8889; charset=utf8', 'root', 'root' );
+    $pdo = new PDO ( 'mysql:dbname=sample_db; host=localhost;port=8889; charset=utf8', 'root', 'root' );
     print '接続に成功しました。';
 } catch ( PDOException $e ) {
     print "接続エラー:{$e->getMessage()}";
--- input_text.php
+++ input_text.php
@@ -1,12 +1,23 @@
 <?php
-require_once "connect_db.php";
+//データベースに接続
+require_once('connect_db.php');
+
 
 $question = $_POST["question"];
 $reason = $_POST["reason"];
-$values = $_POST["values"];
+$value = $_POST["value"];
 
 echo $question;
-echo $reason;
-echo $values;
+//データベースへ入力内容を登録
+$stmt = $pdo->query("SELECT * FROM sable_tbl ");
+echo $question;
+$stmt = $pdo -> prepare("INSERT INTO sample_tbl (question, reason, value) VALUES (:question, :reason, :value)");
+$stmt->bindParam(':question', $question, PDO::PARAM_STR);
+$stmt->bindParam(':reason', $reason, PDO::PARAM_STR);
+$stmt->bindParam(':value', $value, PDO::PARAM_INT);
+$stmt->execute();
+
+//データベース切断
+$pdo = null;
 
  ?>
