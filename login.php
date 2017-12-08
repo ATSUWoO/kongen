5989d2c1c482c88ed913c85cc60f11a3b4eab238
正野敦也 <ShonoAtsuya@setalabnoMac-mini.local>
2017-12-08 15:11:04 +0900

    git 確認

--- login.php
+++ login.php
@@ -1,38 +1,39 @@
 <?php
 
-require_once('config.php');
-
  // セッション開始
 session_start();
  // 既にログインしている場合にはメインページに遷移
- if (isset($_SESSION["USERID"])) {
+ if (isset($_SESSION["email"])) {
 header('Location: index.html');
  exit;
  }
-$db['host'] = 'localhost';
+
+$db['host'] = '127.0.0.1';
 $db['user'] = 'root';
 $db['pass'] = 'root';
-$db['dbname'] = 'testData';
+$db['dbname'] = 'sample_db';
 $error = '';
  // ログインボタンが押されたら
- if (isset($_POST['login'])) {
- if (empty($_POST['username'])) {
+
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ if (empty($_POST['email'])) {
 $error = 'ユーザーIDが未入力です。';
  } else if (empty($_POST['password'])) {
 $error = 'パスワードが未入力です。';
  }
- if (!empty($_POST['username']) && !empty($_POST['password'])) {
-$username = $_POST['username'];
-$dsn = sprintf('mysql:dbname = sample_db; host=localhost;port = 8889; charset=utf8', $db['host'], $db['dbname']);
+ if (!empty($_POST['email']) && !empty($_POST['password'])) {
+$username = $_POST['email'];
+$dsn = sprintf('mysql:dbname = sample_db; host= 127.0.0.1 ;port = 8889; charset=utf8');
  try {
 $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-$stmt = $pdo->prepare('SELECT * FROM user WHERE name = ?');
-$stmt->execute(array($username));
+$stmt = $pdo->prepare('SELECT * FROM users WHERE name = ?');
+$sth->bindValue(1 , "kanimiso");
+$stmt->execute();
 $password = $_POST['password'];
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
  if (password_verify($password, $result['password'])) {
-$_SESSION['USERID'] = $username;
-header('Location: main.php');
+$_SESSION['email'] = $email;
+header('Location: index　.html');
  exit();
  } else {
 $error = 'ユーザーIDあるいはパスワードに誤りがあります。';
@@ -42,6 +43,4 @@ echo $e->getMessage();
  }
  }
  }
-?>
-
 ?>
