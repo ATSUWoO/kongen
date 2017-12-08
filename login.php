403f42e28da12783d6d75604dd4c348d4c88dc75
正野　敦也 <ATSUYA@masanoatsuya-no-MacBook-Air.local>
2017-12-08 15:17:56 +0900

    commit before merge

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
 
-require_once('config.php');
-
  // セッション開始
 session_start();
  // 既にログインしている場合にはメインページに遷移
- if (isset($_SESSION["USERID"])) {
+ if (isset($_SESSION["email"])) {
 header('Location: index.html');
  exit;
  }
@@ -16,13 +14,13 @@ $db['dbname'] = 'testData';
 $error = '';
  // ログインボタンが押されたら
  if (isset($_POST['login'])) {
- if (empty($_POST['username'])) {
+ if (empty($_POST['email'])) {
 $error = 'ユーザーIDが未入力です。';
  } else if (empty($_POST['password'])) {
 $error = 'パスワードが未入力です。';
  }
- if (!empty($_POST['username']) && !empty($_POST['password'])) {
-$username = $_POST['username'];
+ if (!empty($_POST['email']) && !empty($_POST['password'])) {
+$username = $_POST['email'];
 $dsn = sprintf('mysql:dbname = sample_db; host=localhost;port = 8889; charset=utf8', $db['host'], $db['dbname']);
  try {
 $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
@@ -31,8 +29,8 @@ $stmt->execute(array($username));
 $password = $_POST['password'];
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
  if (password_verify($password, $result['password'])) {
-$_SESSION['USERID'] = $username;
-header('Location: main.php');
+$_SESSION['email'] = $email;
+header('Location: index.html');
  exit();
  } else {
 $error = 'ユーザーIDあるいはパスワードに誤りがあります。';
@@ -42,6 +40,4 @@ echo $e->getMessage();
  }
  }
  }
-?>
-
 ?>
